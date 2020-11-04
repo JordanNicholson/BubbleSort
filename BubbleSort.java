a51e20b79cb8e1c10530081e32b81aa8cf492a76
JordanNicholson <71711661+JordanNicholson@users.noreply.github.com>
2020-11-04 10:34:25 -0500

    Spacing

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,47 +8,48 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BubbleSort {
-    public static void main (String [] args) {
-        // Create the input scanner
-        Scanner input = new Scanner(System.in);
+	public static void main (String [] args) {
+		// Create the input scanner
+		Scanner input = new Scanner(System.in);
 
-        // Read in the array size
-        System.err.println("How many numbers are there?");
-        int size = input.nextInt();
+		// Read in the array size
+		System.err.println("How many numbers are there?");
+		int size = input.nextInt();
 
-        // Read in the array elements
-        System.err.println("Enter the numbers:");
-        int [] numbers = new int [size];
-        for (int index = 0; index < size; index++) {
-            numbers[index] = input.nextInt();
-        }
+		// Read in the array elements
+		System.err.println("Enter the numbers:");
+		int [] numbers = new int [size];
+		for (int index = 0; index < size; index++) {
+			numbers[index] = input.nextInt();
+		}
 
-        //Sort the array using Bubble Sort ****** err
-        int temp = 0;
-        for (int index = 0; index < size; index++) {
-        	
-        	System.out.print("Before pass " + (index + 1) + ": \n");
-        	System.out.println(Arrays.toString(numbers));
-        	
-        	for (int secondIndex = 1; secondIndex < size; secondIndex++) {
-        		//if this if loop runs, boolean returns true, and break	
-        		if (numbers[secondIndex - 1] > numbers[secondIndex]) {
-        			
-        			temp = numbers[secondIndex - 1];
-        			numbers[secondIndex - 1] = numbers[secondIndex];
-        			numbers[secondIndex] = temp;
-   
-        		}
-        		
-        	}
-        	
-        }
-        
+		//Sort the array using Bubble Sort ****** err
+		int temp = 0;
+		for (int index = 0; index < size; index++) {
 
-        // Print out the sorted array***** err
-        System.out.println("Sorted Array:");
-        System.out.println(Arrays.toString(numbers));
-		
+			System.out.print("Before pass " + (index + 1) + ": \n");
+			System.out.println(Arrays.toString(numbers));
+
+			for (int secondIndex = 1; secondIndex < size; secondIndex++) {
+				//if this if loop runs, boolean returns true, and break	
+				//boolean condition = 
+				if (numbers[secondIndex - 1] > numbers[secondIndex]) {
+
+					temp = numbers[secondIndex - 1];
+					numbers[secondIndex - 1] = numbers[secondIndex];
+					numbers[secondIndex] = temp;
+
+				}
+
+			}
+
+		}
+
+
+		// Print out the sorted array***** err
+		System.out.println("Sorted Array:");
+		System.out.println(Arrays.toString(numbers));
+
 	}
 }
 
