78ccc31dc55cf31ee40e7376f3369744ae2b4fd6
JordanNicholson <71711661+JordanNicholson@users.noreply.github.com>
2020-11-12 15:28:31 -0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,6 @@
 /*
  * Programmer: Jordan Nicholson
- * Date: November 2, 2020
+ * Date: November 4, 2020
  * Purpose: To bubble sort an array and print the array after each pass.
  */
 
@@ -9,6 +9,52 @@ import java.util.Scanner;
 
 public class BubbleSort {
 	public static void main (String [] args) {
+		/*
+
+		//Sort the array using Bubble Sort
+		int temp = 0;
+		for (int index = 0; index < size; index++) {
+
+			System.out.print("Before pass " + (index + 1) + ": \n");
+			System.out.println(Arrays.toString(numbers));
+		}
+
+		boolean swapped;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+
+			if(numbers[i] > numbers[j]) {
+				swapped = true;
+				temp = numbers[i - 1];
+				numbers[i - 1] = numbers[i];
+				numbers[i] = temp;
+			}
+			else {
+				swapped = false;
+			}
+
+				if (swapped == false) {
+					break;
+				}
+
+
+		}
+		}
+
+
+//if this if loop runs, boolean returns true, and break	
+				//boolean condition = 
+
+
+		// Print out the sorted array
+		System.out.println("Sorted Array:");
+		System.out.println(Arrays.toString(numbers));
+
+	}
+}
+		 */
+
 		// Create the input scanner
 		Scanner input = new Scanner(System.in);
 
@@ -23,26 +69,25 @@ public class BubbleSort {
 			numbers[index] = input.nextInt();
 		}
 
-		//Sort the array using Bubble Sort
+		//Print messages to the error stream
 		int temp = 0;
 		for (int index = 0; index < size; index++) {
 
 			System.err.print("Before pass " + (index + 1) + ": \n");
 			System.out.println(Arrays.toString(numbers));
 
+
 			for (int secondIndex = 1; secondIndex < size; secondIndex++) {
-				//if this if loop runs, boolean returns true, and break	
-				//boolean condition = 
+
+				//Sort the array using Bubble Sort 
 				if (numbers[secondIndex - 1] > numbers[secondIndex]) {
 
 					temp = numbers[secondIndex - 1];
 					numbers[secondIndex - 1] = numbers[secondIndex];
 					numbers[secondIndex] = temp;
-
 				}
 
 			}
-
 		}
 
 
@@ -52,4 +97,3 @@ public class BubbleSort {
 
 	}
 }
-
